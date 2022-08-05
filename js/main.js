1d52b8678ffc1afcde3d5ebb1ea99e03434612ae
NDelan <nutakordelanyo2@gmail.com>
2022-08-05 23:34:45 +0000

    show and exit menu feature by js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Menu show Y hidden
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+// MENU SHOW
+// validate if constant exists
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+// MENU HIDDEN
+// validate if constant exists
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('nav_link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each list, we remove show menu class
+
+}
